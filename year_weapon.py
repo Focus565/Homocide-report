253d50fa2a0e915cd39fa52e94df0ff3f5f79e2a
Chaiyapong Nithipeerawat <Sullvo1147@hotmail.com>
2017-12-11 18:45:33 +0700

    allweapon in year

--- year_weapon.py
+++ year_weapon.py
@@ -16,6 +16,14 @@ def year():
     gun_year = []
     knife_year = []
     cout_knife_year = []
+    blunt = []
+    blunt_year = []
+    strangu_suffo = []
+    strangu_suffo_year = []
+    fire = []
+    fire_year = []
+    other = []
+    other_year = []
 
 
     for year in data['Year']:
@@ -32,11 +40,39 @@ def year():
         if weapons[cout] == 'Knife':
             knife_year.append(i)
         cout += 1
+    cout = 0
+    for i in homocide_in_year:
+        if weapons[cout] == 'Blunt Object':
+            blunt.append(i)
+        cout += 1
+    cout = 0
+    for i in homocide_in_year:
+        if weapons[cout] in ['Strangulation', 'Suffocation', 'Drowning']:
+            strangu_suffo.append(i)
+        cout += 1
+    cout = 0
+    for i in homocide_in_year:
+        if weapons[cout] == 'Fire':
+            fire.append(i)
+        cout += 1
+    cout = 0
+    for i in homocide_in_year:
+        if weapons[cout] in ['Drugs', 'Explosives', 'Poison', 'Fall']:
+            other.append(i)
+        cout += 1
 
     for count in range(1980, 2015):
         years.append(gun_year.count(count))
     for count in range(1980, 2015):
         cout_knife_year.append(knife_year.count(count))
+    for count in range(1980, 2015):
+        blunt_year.append(blunt.count(count))
+    for count in range(1980, 2015):
+        strangu_suffo_year.append(strangu_suffo.count(count))
+    for count in range(1980, 2015):
+        fire_year.append(fire.count(count))
+    for count in range(1980, 2015):
+        other_year.append(other.count(count))
 
     bar = pygal.Line(style=DarkStyle,title='Homocide in year kill by firearm', \
         show_minor_x_labels=False, x_label_rotation=20)
@@ -45,5 +81,9 @@ def year():
     '1995', '2000','2005', '2010', '2014']
     bar.add('Firearm', years)
     bar.add('Knife', cout_knife_year)
+    bar.add('Blunt Object', blunt_year)
+    bar.add('Strangulation or Suffocation', strangu_suffo_year)
+    bar.add('Fire', fire_year)
+    bar.add('Others', other_year)
     bar.render_to_file('img/year_Weapon.svg')
 year()
